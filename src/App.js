16b7a64b264ea5b35fb2e393a6ec3812a568528f
pruthvi <pruthvirajbarbole55@gmail.com>
2024-03-27 18:24:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import Cart from './Cart';
+import "./App.css"
+
+const Products = [
+  { id: 1, name: 'Product-1', price: 100 },
+  { id: 2, name: 'Product-2', price: 200 },
+  { id: 3, name: 'Product-3', price: 300 },
+];
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    const existingItem = cart.find(item => item.product.id === product.id);
+    if (existingItem) {
+      setCart(cart.map(item => item.product.id === product.id ? {...item, quantity: item.quantity + 1} : item));
+    } else {
+      setCart([...cart, { product: product, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (product) => {
+    const existingItem = cart.find(item => item.product.id === product.id);
+    if (existingItem.quantity === 1) {
+      setCart(cart.filter(item => item.product.id !== product.id));
+    } else {
+      setCart(cart.map(item => item.product.id === product.id ? {...item, quantity: item.quantity - 1} : item));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductList products={Products} onAddToCart={addToCart} />
+      <Cart cart={cart} onRemoveFromCart={removeFromCart} />
     </div>
   );
 }
